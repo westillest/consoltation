a93f3cf2b8bb87836e2f1add2bafa77fd8cf420b
westillest <73331147+westillest@users.noreply.github.com>
2021-11-21 22:48:23 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+print("what is your name ?")
+
+
+
+def consultation():
+    x = input()
+    print ("hey" +" "+ x +" "+ "this is your online medical service!")
+
+consultation()
+
+
+print ("what are some of the symptoms you've been having so that i see how i may best help you!")
+
+def symptoms():
+    v ="explain some of the symptoms that you having"
+    print(v)
+    signs = input()
+
+symptoms()
+
+
+def solution():
+    if (symptoms() = "headache, nausea,pain in joints"):
+        solution()
+
+
+"""from  selenium import webdriver
+
+PATH = ("C:\Program Files (x86)\chromedriver")
+
+driver = webdriver.chrome(PATH)
+
+driver.get("https/:www.instagram.com")"""
+
+
+
+
+
+
+
+
+
+
